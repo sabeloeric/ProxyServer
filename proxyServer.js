2f3666525e909cb403db8b172512111bba830c33
Sabelo Thabethe <thabethesabelo@gmail.com>
2023-10-03 16:01:46 +0200

    Add places route

--- proxyServer.js
+++ proxyServer.js
@@ -10,7 +10,22 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.get('/proxy', (req, res) => {
-  const url = req.query.url; 
+  const url = req.query.url;
+
+  request(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      res.send(body);
+    } else {
+      res.status(500).send('Error fetching data');
+    }
+  });
+});
+
+app.get('/places', (req, res) => {
+  const query = req.query.query;
+  const apiKey = req.query.apiKey;
+
+  const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${query}&language=en&key=${apiKey}`;
 
   request(url, (error, response, body) => {
     if (!error && response.statusCode === 200) {
