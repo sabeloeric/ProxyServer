f0457cad8ecd010181e7364c37c3f7066128375e
Sabelo Thabethe <thabethesabelo@gmail.com>
2023-10-04 07:39:54 +0200

    Add console logs

--- proxyServer.js
+++ proxyServer.js
@@ -12,6 +12,8 @@ app.use(cors(corsOptions));
 app.get('/proxy', (req, res) => {
   const url = req.query.url;
 
+  print({url});
+
   request(url, (error, response, body) => {
     if (!error && response.statusCode === 200) {
       res.send(body);
@@ -23,6 +25,12 @@ app.get('/proxy', (req, res) => {
 
 app.get('/proxy/google/', (req, res) => {
   const url = "https://maps.googleapis.com/maps/api" + req.query.url;
+
+  print({
+    url,
+    query: req.query
+  });
+  
 
   request(url, (error, response, body) => {
     if (!error && response.statusCode === 200) {
